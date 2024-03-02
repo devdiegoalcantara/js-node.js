01f455867963aed665b2a690dfe6078f3662c36a
Diego Alcântara <108950234+devdiegoalcantara@users.noreply.github.com>
2024-03-01 23:30:14 -0300

    finish

---
+++ funcoes.js
@@ -0,0 +1,7 @@
+function exibeInfosEstudantes (nome, nota) {
+    return `o nome é ${nome} e a nota é ${nota}`
+}
+
+console.log(exibeInfosEstudantes("Ana", 10));
+
+console.log(exibeInfosEstudantes("Caroline", 7));
