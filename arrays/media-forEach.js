ac1a2ff86bd8a85b80b60fc01de4c9e88f49cc89
Diego Alcântara <108950234+devdiegoalcantara@users.noreply.github.com>
2024-03-02 14:49:55 -0300

    map

--- arrays/media-forEach.js
+++ arrays/media-forEach.js
@@ -9,3 +9,14 @@ notas.forEach(function (nota) {
 const media = somaDasNotas / notas.length;
 
 console.log(`A média das notas é ${media}.`);
+
+// Um array de números
+const numeros = [1, 2, 3, 4, 5];
+
+// Uma função de callback que imprime o dobro do valor
+function dobrar(valor) {
+  console.log(valor * 2);
+}
+
+// Usando o forEach para percorrer o array e chamar a função de callback
+numeros.forEach(dobrar); // Imprime 2, 4, 6, 8, 10
